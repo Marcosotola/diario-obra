c8478b6acd1557048eb231855716434d775bca9b
Marcosotola <111930679+Marcosotola@users.noreply.github.com>
2023-06-03 22:13:21 -0300

    creacion pagina fotos, conexion a db.fotos

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const axios = require("axios");
 require("dotenv").config();
 const admin = require("firebase-admin");
 
+
 const serviceAccount = require("./firebase.json"); // el archivo de credenciales de firebase
 const port = 3000;
 
@@ -11,6 +12,7 @@ const port = 3000;
 const app = express();
 
 app.use(express.static("public"));
+
 
 //Configurar el directorio de vistas y el motor de plantilla
 app.set("views", __dirname + "/views");
@@ -23,28 +25,38 @@ admin.initializeApp({
 
 
 const db = admin.firestore();
-const tareasCollection = db.collection('tareas').orderBy("fecha","asc");
+const tareasCollection = db.collection('tareas').orderBy("fecha", "asc");
 const tareasEditCreate = db.collection('tareas');
+const fotosCollection = db.collection('fotos');
 
 app.set('view engine', 'hbs');
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
 
-    app.get('/', async (req, res) => {
+
+app.get('/', async (req, res) => {
+
     const tareasSnapshot = await tareasCollection.get();
     const tareas = tareasSnapshot.docs.map(doc => ({
         id: doc.id,
         ...doc.data()
     }));
     res.render('index', { tareas });
-    });
+});
 
-
+app.get('/fotos', async (req, res) => {
+    const fotosSnapshot = await fotosCollection.get();
+    const fotos = fotosSnapshot.docs.map(doc => ({
+        id: doc.id,
+        ...doc.data()
+    }));
+    res.render('fotos', { fotos });
+});
 
 app.get("/tareas/create", (req, res) => {
     res.render("create");
-    });
+});
 
 app.post("/tareas/create", async (req, res) => {
     const { fecha, nombre, descripcion } = req.body;
@@ -55,8 +67,7 @@ app.post("/tareas/create", async (req, res) => {
     };
     await tareasEditCreate.add(tarea);
     res.redirect("/");
-    });
-
+});
 
 app.get("/tareas/edit/:id", async (req, res) => {
     const tareaId = req.params.id;
@@ -66,34 +77,30 @@ app.get("/tareas/edit/:id", async (req, res) => {
         ...tareasSnapshot.data(),
     };
     res.render("edit", { tarea });
-    });
+});
 
 app.post("/tareas/edit/:id", async (req, res) => {
     const tareaId = req.params.id;
     const { fecha, nombre, descripcion } = req.body;
-
     const tarea = {
         fecha,
         nombre,
         descripcion,
     };
-
     await tareasEditCreate.doc(tareaId).update(tarea);
     res.redirect('/');
+});
 
-    });
+app.get("/tareas/delete/:id", async (req, res) => {
+    const tareaId = req.params.id;
+    await tareasEditCreate.doc(tareaId).delete();
+    res.redirect('/');
+});
 
-    app.get("/tareas/delete/:id", async (req, res) => {
-        const tareaId = req.params.id;
 
-        await tareasEditCreate.doc(tareaId).delete();
 
-        res.redirect('/');
 
-        
-        })
 
-        
 
 
 /*const app = express();
@@ -141,6 +148,6 @@ app.get("/", (req, res) => {
     }
     });
 */
-    app.listen(port, () => {
+app.listen(port, () => {
     console.log("Servidor iniciado en http://localhost:3000 ");
-    });
+});
