81b7ac2cbe21bd2ba2f680f3548e16c18875afdc
Marcosotola <111930679+Marcosotola@users.noreply.github.com>
2023-06-09 04:19:22 -0300

    footer

--- controllers/perrosController.js
+++ controllers/perrosController.js
@@ -1,8 +1,10 @@
 
 const axios = require("axios");
+require('dotenv').config({ path: '../env' });
 
 const logUrlMiddleware = async (req, res, next) => {
   try {
+    const apiKey = process.env.API_KEY;
     const respuesta = await axios.get('https://random.dog/woof.json');
     const urlPerro = respuesta.data.url;
     req.urlPerro = urlPerro;
