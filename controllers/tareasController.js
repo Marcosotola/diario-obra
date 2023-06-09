8a85f7499a4427ad33ed44ee14e7089ebf043840
Marcosotola <111930679+Marcosotola@users.noreply.github.com>
2023-06-09 03:05:45 -0300

    modularizado del CRUD

--- controllers/tareasController.js
+++ controllers/tareasController.js
@@ -1,11 +1,9 @@
-
 const { getFirestoreInstance } = require("../firebase");
 const db = getFirestoreInstance();
-
-
+const express = require('express');
+const router = express.Router();
 
 const tareasCollection = db.collection('tareas').orderBy("fecha", "asc");
-
 
 const index = async (req, res) => {
     const tareasSnapshot = await tareasCollection.get();
@@ -16,14 +14,55 @@ const index = async (req, res) => {
     res.render('index', { tareas });
 };
 
+const tareasEditCreate = db.collection('tareas');
 
+router.get("/create", (req, res) => {
+    res.render("create");
+});
+
+router.post("/create", async (req, res) => {
+    const { fecha, nombre, descripcion } = req.body;
+    const tarea = {
+        fecha,
+        nombre,
+        descripcion,
+    };
+    await tareasEditCreate.add(tarea);
+    res.redirect("/");
+});
+
+router.get("/edit/:id", async (req, res) => {
+    const tareaId = req.params.id;
+    const tareasSnapshot = await tareasEditCreate.doc(tareaId).get();
+    const tarea = {
+        id: tareasSnapshot.id,
+        ...tareasSnapshot.data(),
+    };
+    res.render("edit", { tarea });
+});
+
+router.post("/edit/:id", async (req, res) => {
+    const tareaId = req.params.id;
+    const { fecha, nombre, descripcion } = req.body;
+    const tarea = {
+        fecha,
+        nombre,
+        descripcion,
+    };
+    await tareasEditCreate.doc(tareaId).update(tarea);
+    res.redirect('/');
+});
+
+router.get("/delete/:id", async (req, res) => {
+    const tareaId = req.params.id;
+    await tareasEditCreate.doc(tareaId).delete();
+    res.redirect('/');
+});
 
 module.exports = {
-    index
+    index,
+    router
 };
-
-
-
 
 
 
--- routes/tareasRoutes.js
+++ routes/tareasRoutes.js
@@ -2,12 +2,8 @@ const express = require('express');
 const router = express.Router();
 const tareasController = require('../controllers/tareasController');
 
+router.use('/tareas', tareasController.router); 
 
 router.get('/', tareasController.index);
-
-
-
-
-
 
 module.exports = router;
