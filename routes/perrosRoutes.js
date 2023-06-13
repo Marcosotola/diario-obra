6492496ae521f4a3e935dcde7ae5c8f08cef8e84
Marcosotola <111930679+Marcosotola@users.noreply.github.com>
2023-06-12 22:01:23 -0300

    quitando api de perros

--- routes/perrosRoutes.js
+++
@@ -1,9 +0,0 @@
-// perrosRoutes.js
-const express = require("express");
-const router = express.Router();
-const perrosController = require("../controllers/perrosController");
-
-router.get("/", perrosController.logUrlMiddleware, perrosController.show);
-
-module.exports = router;
-
